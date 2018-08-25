4d5cff266e7ddc21c4c6c81691e39d596aa3dec2
Amilcarro <arrecinos.ar@gmail.com>
2018-08-24 23:41:51 -0600

    Sales

---
+++ Sales.Backend/Controllers/PartsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sales.Backend.Models;
+using Sales.Common.Models;
+
+namespace Sales.Backend.Controllers
+{
+    public class PartsController : Controller
+    {
+        private LocalDataContext db = new LocalDataContext();
+
+        // GET: Parts
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Part.ToListAsync());
+        }
+
+        // GET: Parts/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Part.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // GET: Parts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Description,Stock_Um,Primary_Whs_Id,Primary_Loc_Id,Unit_Material_Cost,STATUS_EFF_DATE")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Part.Add(part);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(part);
+        }
+
+        // GET: Parts/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Part.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Parts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Description,Stock_Um,Primary_Whs_Id,Primary_Loc_Id,Unit_Material_Cost,STATUS_EFF_DATE")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(part).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(part);
+        }
+
+        // GET: Parts/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Part.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Parts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Part part = await db.Part.FindAsync(id);
+            db.Part.Remove(part);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sales.Common/Models/Part.cs
@@ -0,0 +1,24 @@
+﻿namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Part
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string Stock_Um { get; set; }
+
+        public string Primary_Whs_Id { get; set; }
+            
+        public string Primary_Loc_Id { get; set; }
+
+        public decimal Unit_Material_Cost { get; set; }
+
+        public DateTime STATUS_EFF_DATE { get; set; }   
+
+    }
+}
